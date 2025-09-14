fix tensor broadcast mismatch

models/vae/decoder_t.py
@@ -18,6 +18,7 @@ class ReplayDecoderT(nn.Module):
         self,
         latent_dim: int,
         transformer_args: TransformerArgs = None,
+        seq_len: int = SEQ_LEN,
         past_frames: int = 0,
         future_frames: int = 0,
     ):
@@ -31,7 +32,7 @@ class ReplayDecoderT(nn.Module):
         embed_dim = self.transformer_args.embed_dim
 
         self.pos_dec = nn.Parameter(
-            torch.randn(SEQ_LEN, self.transformer_args.embed_dim)
+            torch.randn(seq_len, self.transformer_args.embed_dim)
         )
 
         self.pos_head = nn.Sequential(